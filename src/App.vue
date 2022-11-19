50fece26847e2a8bb4e3ba4703d8e953fa08b365
Даниил <fuzeev.daniil@yandex.ru>
2022-11-19 15:06:02 +0300

    feat: add cart-list

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,102 @@
           <span class="header-cart__amount">4 товара</span>
           <button class="header-cart__btn">Очистить корзину</button>
         </div>
+        <div class="cart__content content-cart">
+          <div class="content-cart__list cart-list">
+            <div class="cart-list__item cart-item">
+              <div class="cart-item__img">
+                <img
+                  src="./assets/images/img/G2H.png"
+                  alt="G2H"
+                  width="100"
+                  height="100"
+                />
+              </div>
+              <div class="cart-item__text item-text">
+                <h4 class="item-text__title">Вытяжное устройство G2H</h4>
+                <p class="item-text__description">
+                  12-72/168 м<sup>3</sup>/ч / гидрорегулируемый расход / от
+                  датчика присутствия
+                </p>
+                <p class="item-text__article">Артикул: G2H1065</p>
+              </div>
+              <div class="cart-item__amount item-amount">
+                <button class="item-amount__decrement item-counter"></button>
+                <span class="item-amount__count item-counter">1</span>
+                <button class="item-amount__increment item-counter"></button>
+                <span class="item-amount__price">12 644 ₽/шт.</span>
+              </div>
+              <div class="cart-item__price">12 644 ₽</div>
+              <button class="cart-item__btn"></button>
+            </div>
+            <div class="cart-list__item cart-item">
+              <div class="cart-item__img">
+                <img
+                  src="./assets/images/img/G2H.png"
+                  alt="G2H"
+                  width="100"
+                  height="100"
+                />
+              </div>
+              <div class="cart-item__text item-text">
+                <h4 class="item-text__title">Вытяжное устройство G2H</h4>
+                <p class="item-text__description">
+                  12-72/168 м<sup>3</sup>/ч / гидрорегулируемый расход / от
+                  датчика присутствия
+                </p>
+                <p class="item-text__article">Артикул: G2H1065</p>
+              </div>
+              <div class="cart-item__amount item-amount">
+                <button class="item-amount__decrement item-counter"></button>
+                <span class="item-amount__count item-counter">1</span>
+                <button class="item-amount__increment item-counter"></button>
+                <span class="item-amount__price">12 644 ₽/шт.</span>
+              </div>
+              <div class="cart-item__price">12 644 ₽</div>
+              <button class="cart-item__btn"></button>
+            </div>
+            <div class="cart-list__item cart-item">
+              <div class="cart-item__img">
+                <img
+                  src="./assets/images/img/G2H.png"
+                  alt="G2H"
+                  width="100"
+                  height="100"
+                />
+              </div>
+              <div class="cart-item__text item-text">
+                <h4 class="item-text__title">Вытяжное устройство G2H</h4>
+                <p class="item-text__description">
+                  12-72/168 м<sup>3</sup>/ч / гидрорегулируемый расход / от
+                  датчика присутствия
+                </p>
+                <p class="item-text__article">Артикул: G2H1065</p>
+              </div>
+              <div class="cart-item__amount item-amount">
+                <button class="item-amount__decrement item-counter"></button>
+                <span class="item-amount__count item-counter">1</span>
+                <button class="item-amount__increment item-counter"></button>
+                <span class="item-amount__price">12 644 ₽/шт.</span>
+              </div>
+              <div class="cart-item__price">12 644 ₽</div>
+              <button class="cart-item__btn"></button>
+            </div>
+            <div class="cart-list__service service-cart">
+              <label class="service-cart__checkbox">
+                <input class="checkbox" type="checkbox" />
+              </label>
+              <div class="service-cart__img"></div>
+              <div class="service-cart__text service-text">
+                <h5 class="service-text__title">Установка</h5>
+                <p class="service-text__description">
+                  Отметьте, если Вам необходима консультация профессионала по
+                  монтажу выбранных товаров.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="content-cart__total"></div>
+        </div>
       </section>
       <section class="slider"></section>
     </div>

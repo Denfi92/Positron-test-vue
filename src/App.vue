36cd42661f95f31d171b368d765a96537451ebef
Даниил <fuzeev.daniil@yandex.ru>
2022-11-19 12:24:30 +0300

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>1</div>
+  <header class="header">
+    <div class="header__container container">
+      <HeaderTotal />
+    </div>
+  </header>
+  <main class="main">
+    <div class="main__container container">
+      <nav class="main__nav nav-main">
+        <h5 class="nav-main__title">Главная</h5>
+        <ul class="nav-main__list">
+          <li class="nav-main__item">Корзина</li>
+        </ul>
+      </nav>
+      <section class="cart">
+        <div class="cart__header header-cart">
+          <h1 class="header-cart__title">Ваша корзина</h1>
+          <span class="header-cart__amount">4 товара</span>
+          <button class="header-cart__btn">Очистить корзину</button>
+        </div>
+      </section>
+      <section class="slider"></section>
+    </div>
+  </main>
 </template>
 
 <script>
+import HeaderTotal from "./components/HeaderTotal.vue";
 export default {
   name: "App",
-  components: {},
+  components: { HeaderTotal },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 
-import "./assets/scss/index.scss";
+import "./assets/scss/style.scss";
 
 createApp(App).use(store).mount("#app");
